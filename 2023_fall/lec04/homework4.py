6c637e2b82a4a56110c3c14a6b06866f969916f0
qw312108 <76547261+qw312108@users.noreply.github.com>
2023-11-06 14:37:53 +0800

    homework

--- 2023_fall/lec04/homework4.py
+++ 2023_fall/lec04/homework4.py
@@ -1,5 +1,8 @@
 
 def list_to_dict(input_list):
+    dict={index: value for index, value in enumerate(input_list)}
+    return dict
+
     '''
     This function should return a dictionary in which each element of 
     `input_list` is a value, and the corresponding key is the numerical 
